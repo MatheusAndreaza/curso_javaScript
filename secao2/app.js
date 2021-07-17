561cc212c2771730e5a32e110ca7b148a14a26c3
Matheus Andreaza <matheus20203040@hotmail.com>
2021-07-17 20:41:52 -0300

    secao 2

---
+++ secao2/app.js
@@ -0,0 +1,10 @@
+let nome = "Manoel";
+let sobrenome = "Silva";
+let idade = 24;
+const numCliente = 09839842;
+let valorEmprestimo = 150000;
+let taxaDeJuros = 10;
+let ehBomPagador = "SIM";
+ehBomPagador = true;
+
+console.log(`Boa tarde Sr. ${nome} ${sobrenome}. Sua idade é : ${idade}`)
---
+++ secao2/desafios/desafio1.js
@@ -0,0 +1,7 @@
+let nome = "Toddy";
+let preco = 6.00;
+let validade = true;
+
+console.log(`Nome: ${nome}`);
+console.log(`Preço: R$ ${preco}`);
+console.log(`Validade: ${validade}`);
